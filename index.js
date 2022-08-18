8dc27d51818dbc2724629a473690c572c4ca28c5
isan <ihellostereo@gmail.com>
2022-08-18 21:52:43 +0700

    2nd hero commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 const express = require('express')
 const route = require('./routes/router')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('images'))
 // app.use('/images', express.static(process.cwd() + '/images'))
